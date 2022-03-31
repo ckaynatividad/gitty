d47487a143c5b8a06d656e2de8a2d778494cbaaa
ckaynatividad <natividad.ckay@gmail.com>
2022-03-30 17:21:13 -0700

    mocked authenticate so create post is passing

--- __tests__/github.test.js
+++ __tests__/github.test.js
@@ -2,9 +2,19 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const GithubUser = require('../lib/models/GithubUser');
 
 jest.mock('../lib/utils/github');
-
+jest.mock('../lib/middleware/authenticate.js', () => {
+  return (req, res, next) => {
+    req.user = {
+      username: 'omelette',
+      email: 'omelette@example.com',
+      avatar: 'https://www.placecage.com/gif/300/300',
+    };
+    next();
+  };
+});
 describe('gitty routes', () => {
   beforeEach(() => {
     return setup(pool);
@@ -52,5 +62,25 @@ describe('gitty routes', () => {
       message: 'signed out',
       success: true,
     });
+  });
+
+  it('allows user to create post', async () => {
+    const expected = {
+      content: 'test test test',
+    };
+    await GithubUser.insert({
+      username: 'omelette',
+      email: 'omelette@example.com',
+      avatar: 'https://www.placecage.com/gif/300/300',
+    });
+    return request(app)
+      .post('/api/v1/posts')
+      .send(expected)
+      .then((res) => {
+        expect(res.body).toEqual({
+          id: expect.any(String),
+          ...expected,
+        });
+      });
   });
 });
